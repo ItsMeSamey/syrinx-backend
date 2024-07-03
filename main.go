139cbdbd147fd4b1e425aa849c0b9944a161d894
ItsMeSamey <sameychain5041@gmail.com>
2024-07-03 21:11:05 +0530

    reset

--- main.go
+++ main.go
@@ -2,28 +2,10 @@ package main
 
 import (
 	"fmt"
-	
-	"ccs.ctf/DB"
 )
 
 func main() {
-	db, err := DB.OpenDb("2024.ctf")
-	defer db.Close()
-
-	newUser := DB.User{
-		Username: "johnDoe",
-		Password: "PasswordHash", // Will need to write hashing logic
-	}
-	DB.CreateUser(db, newUser)
-
-	username := "johnDoe"
-	password := "secretPassword"
-	authenticatedUser, err := DB.Authenticate(db, username, password)
-	if err != nil {
-		fmt.Println("Authentication failed:", err)
-		return
-	}
-	fmt.Printf("User authenticated: %s\n", authenticatedUser.Username)
+	fmt.Printf("pass\n")
 }
 
 
