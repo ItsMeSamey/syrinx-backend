9ac19f1c3b94578b54aed51d3274fff97149a229
ItsMeSamey <sameychain5041@gmail.com>
2024-07-09 21:43:53 +0530

    cleanup

--- main.go
+++ main.go
@@ -15,17 +15,6 @@ func main() {
   }
 
   // Start serving the front end server
-  err = DB.SendConfirmationEmail(&DB.User{
-    Username: "a",
-    Email: "sanyamsingh504@gmail.com",
-    Password: "adsasdasd",
-    DiscordID: "asdasddas",
-    TeamID: DB.TID(&[3]byte{1,2,3}),
-  })
-  if err != nil {
-    log.Fatal(err)
-  }
-
   Server.Start("127.0.0.1:8080", "../Syrinx_Login/dist/")
 }
 
