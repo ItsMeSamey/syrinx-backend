48d35743176c690728b2b3c94d6e1b78018d81dc
ItsMeSamey <sameychain5041@gmail.com>
2024-07-11 16:38:28 +0530

    teams

--- DB/team.go
+++ DB/team.go
@@ -4,12 +4,17 @@ import (
   "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+/// Database sorted by TeamID
 type Team struct {
-  ID         ObjID  `bson:"_id,omitempty"`
-  TeamID   string `bson:"question"`
+  TeamID   TID `bson:"question"`
   TeamName  string  `bson:"teamName"`
   Points     int    `bson:"points"`
   Solved     map[primitive.ObjectID]int `bson:"solved"`
   Level      int    `bson:"level"`
+}
+
+
+func getTeamNameByTID(id TID) (string, error) {
+  return "", nil
 }
 
