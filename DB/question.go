a3998d579a5015ccd780db153699da87c2e65918
Shreyas Mahajan <shreyasmahajan05@gmail.com>
2024-07-12 06:30:56 +0530

    Co-authored-by: RiteshKtiet <RiteshKtiet@users.noreply.github.com>

--- DB/question.go
+++ DB/question.go
@@ -1,6 +1,8 @@
 package DB
 
 import (
+  "math/rand"
+  "errors"
 )
 
 type Question struct {
@@ -16,5 +18,20 @@ type Question struct {
 func QuestionFromID(_id string) (*Question, error) {
   var question Question
   return &question, QuestionDB.get("_id", _id, &question)
+}
+
+func genQuestionID() (int16, error) {
+  times:=0
+  start: 
+  ID:=int16(rand.Intn(32767))
+  exists, err := QuestionDB.exists("questionID", ID)
+  if exists {
+    if times > 1024 {
+      return 0, errors.New("genQuestionID: Lucky Error!!")
+    }
+    times += 1
+    goto start
+  }
+  return ID, err
 }
 
